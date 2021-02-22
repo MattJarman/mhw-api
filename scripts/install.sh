0ef5f3c28aac207a568f59f0bb440a455dcd6c55
Matthew <mattjarman22@outlook.com>
2021-02-22 16:47:37 +0000

    Update install script, Add run command

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-ROOT_DIR="$(dirname $(realpath "$0"))/.."
+ROOT_DIR="$(dirname "$(realpath "$0")")/.."
+
+[ ! -f .env ] && cp .env.example .env
 
 if [ ! -d "$ROOT_DIR/vendor" ]; then
-    docker run --rm \
-        -v "$ROOT_DIR":/opt \
-        -w /opt \
-        laravelsail/php80-composer:latest \
+    docker-compose run --rm \
+        mhw-php \
         composer install
 fi
 
+source .env
+
+if [ -n "$APP_NAME" ]; then
+    docker-compose run --rm \
+        mhw-php \
+        php artisan key:generate
+fi
